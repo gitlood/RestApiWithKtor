ddb924fedceca89a98c776ac06c81b73e9522420
gitlood <unilood@gmail.com>
2022-12-28 12:43:21 +1300

    Database Setup

---
+++ src/main/kotlin/com/example/Application.kt
@@ -0,0 +1,28 @@
+package com.example
+
+import com.example.plugins.configureRouting
+import io.ktor.serialization.gson.*
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.server.plugins.callloging.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.plugins.defaultheaders.*
+import io.ktor.server.routing.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
+}
+
+fun Application.module() {
+    configureRouting()
+    install(DefaultHeaders)
+    install(CallLogging)
+    install(Routing)
+    install(ContentNegotiation) {
+        gson {
+            setPrettyPrinting()
+        }
+    }
+}
