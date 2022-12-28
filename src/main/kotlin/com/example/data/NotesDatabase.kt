eb0ff7ab5ce4e3fe9c3d7f9b6b15037f5b964241
gitlood <unilood@gmail.com>
2022-12-29 04:38:29 +1300

    Check user exists function

--- src/main/kotlin/com/example/data/NotesDatabase.kt
+++ src/main/kotlin/com/example/data/NotesDatabase.kt
@@ -3,6 +3,7 @@ package com.example.data
 import com.example.data.collections.Note
 import com.example.data.collections.User
 import org.litote.kmongo.coroutine.coroutine
+import org.litote.kmongo.eq
 import org.litote.kmongo.reactivestreams.KMongo
 
 private val client = KMongo.createClient().coroutine
@@ -12,4 +13,8 @@ private val notes = database.getCollection<Note>()
 
 suspend fun registerUser(user: User): Boolean {
     return users.insertOne(user).wasAcknowledged()
+}
+
+suspend fun checkIfUserExists(email: String): Boolean {
+    return users.findOne(User::email eq email) != null
 }
