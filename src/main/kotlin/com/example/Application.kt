f36a4fe65e8b4598ede114e9ea55e35323e2484e
gitlood <unilood@gmail.com>
2022-12-29 05:24:05 +1300

    Authentication Config

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -1,9 +1,11 @@
 package com.example
 
+import com.example.data.checkPasswordForEmail
 import com.example.routes.loginRoute
 import com.example.routes.registerRoute
 import io.ktor.serialization.gson.*
 import io.ktor.server.application.*
+import io.ktor.server.auth.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.server.plugins.callloging.*
@@ -26,6 +28,22 @@ fun Application.module() {
     install(ContentNegotiation) {
         gson {
             setPrettyPrinting()
+        }
+    }
+    install(Authentication) {
+        configureAuth()
+    }
+}
+
+private fun AuthenticationConfig.configureAuth() {
+    basic {
+        realm = "Note Server"
+        validate { credentials ->
+            val email = credentials.name
+            val password = credentials.password
+            if (checkPasswordForEmail(email, password)) {
+                UserIdPrincipal(email)
+            } else null
         }
     }
 }
