6fd3a4b205ccea7fce09ecf0b8ada75d49809daa
gitlood <unilood@gmail.com>
2022-12-29 08:59:57 +1300

    Add Owner to notes

---
+++ src/main/kotlin/com/example/data/requests/AddOwnerRequest.kt
@@ -0,0 +1,6 @@
+package com.example.data.requests
+
+data class AddOwnerRequest(
+    val noteID: String,
+    val owner: String
+)
--- src/main/kotlin/com/example/routes/NoteRoutes.kt
+++ src/main/kotlin/com/example/routes/NoteRoutes.kt
@@ -1,10 +1,10 @@
 package com.example.routes
 
+import com.example.data.*
 import com.example.data.collections.Note
-import com.example.data.deleteNoteForUser
-import com.example.data.getNotesForUser
+import com.example.data.requests.AddOwnerRequest
 import com.example.data.requests.DeleteNoteRequest
-import com.example.data.saveNote
+import com.example.data.responses.SimpleResponse
 import io.ktor.http.HttpStatusCode.Companion.BadRequest
 import io.ktor.http.HttpStatusCode.Companion.Conflict
 import io.ktor.http.HttpStatusCode.Companion.OK
@@ -53,6 +53,37 @@ fun Route.noteRoutes() {
                 }
                 if (deleteNoteForUser(email, request.id)) {
                     call.respond(OK)
+                } else {
+                    call.respond(Conflict)
+                }
+            }
+        }
+    }
+    route("/addOwnerToNote") {
+        authenticate {
+            post {
+                val request = try {
+                    call.receive<AddOwnerRequest>()
+                } catch (e: ContentTransformationException) {
+                    call.respond(BadRequest)
+                    return@post
+                }
+                if (!checkIfUserExists(request.owner)) {
+                    call.respond(
+                        OK,
+                        SimpleResponse(false, "No User with this email exists")
+                    )
+                    return@post
+                }
+                if (isOwnerOfNote(request.noteID, request.owner)) {
+                    call.respond(
+                        OK,
+                        SimpleResponse(false, "This user is already an owner of this note")
+                    )
+                    return@post
+                }
+                if (addOwnerToNote(request.noteID, request.owner)) {
+                    call.respond(OK, SimpleResponse(true, "${request.owner} can now see this note"))
                 } else {
                     call.respond(Conflict)
                 }
