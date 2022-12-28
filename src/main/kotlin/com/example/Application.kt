a4645b7134993ffeba288ae1d754d9a858a1e025
gitlood <unilood@gmail.com>
2022-12-29 04:58:55 +1300

    Register User with error handling

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -1,8 +1,6 @@
 package com.example
 
-import com.example.data.collections.User
-import com.example.data.registerUser
-import com.example.plugins.configureRouting
+import com.example.routes.registerRoute
 import io.ktor.serialization.gson.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
@@ -10,9 +8,7 @@ import io.ktor.server.netty.*
 import io.ktor.server.plugins.callloging.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.plugins.defaultheaders.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import io.ktor.server.routing.*
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
@@ -20,17 +16,14 @@ fun main() {
 }
 
 fun Application.module() {
-    configureRouting()
     install(DefaultHeaders)
     install(CallLogging)
+    install(Routing) {
+        registerRoute()
+    }
     install(ContentNegotiation) {
         gson {
             setPrettyPrinting()
         }
-    }
-    CoroutineScope(Dispatchers.IO).launch {
-        registerUser(
-            User(email = "abc@abc.com", password = "123456")
-        )
     }
 }
---
+++ src/main/kotlin/com/example/data/requests/AccountRequest.kt
@@ -0,0 +1,6 @@
+package com.example.data.requests
+
+data class AccountRequest(
+    val email: String,
+    val password: String
+)
