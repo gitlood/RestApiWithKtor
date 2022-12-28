785c9c9c845842807a8c2d30e674e63ad40913c9
gitlood <unilood@gmail.com>
2022-12-29 05:52:55 +1300

    Add Note implemented

--- src/main/kotlin/com/example/routes/NoteRoutes.kt
+++ src/main/kotlin/com/example/routes/NoteRoutes.kt
@@ -1,9 +1,14 @@
 package com.example.routes
 
+import com.example.data.collections.Note
 import com.example.data.getNotesForUser
+import com.example.data.saveNote
+import io.ktor.http.HttpStatusCode.Companion.BadRequest
+import io.ktor.http.HttpStatusCode.Companion.Conflict
 import io.ktor.http.HttpStatusCode.Companion.OK
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
@@ -14,6 +19,23 @@ fun Route.noteRoutes() {
                 val email = call.principal<UserIdPrincipal>()!!.name
                 val notes = getNotesForUser(email)
                 call.respond(OK, notes)
+            }
+        }
+    }
+    route("/addNote") {
+        authenticate {
+            post {
+                val note = try {
+                    call.receive<Note>()
+                } catch (e: ContentTransformationException) {
+                    call.respond(BadRequest)
+                    return@post
+                }
+                if (saveNote(note)) {
+                    call.respond(OK)
+                } else {
+                    call.respond(Conflict)
+                }
             }
         }
     }
